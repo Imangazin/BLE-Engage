f487b19fc37a7914a053f5469fe53f80036c112f
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-03-19 11:10:58 -0400

    test

--- src/toolInteract.php
+++ src/toolInteract.php
@@ -12,10 +12,10 @@ if (isSafari()) session_id($_POST["session_id"]);
 session_start();
 
 if($_SESSION['_basic_lti_context']['oauth_consumer_key'] == $lti_auth['key']){
-    echo "test";
     $orgUnitId = $_SESSION['_basic_lti_context']['context_id'];
     $userName = $_SESSION['_basic_lti_context']['ext_d2l_username'];
     if (isset($_GET['organizationId'])){
+        echo $_GET['organizationId'];
         $events_response = getEvents($_GET['organizationId']);
         echo json_decode($events_response);
     }else{
