25e203ee422ab590948f8bff0b7e631638035b26
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-03-27 15:00:29 -0400

    test

--- src/BLE.php
+++ src/BLE.php
@@ -39,10 +39,12 @@ function isSectionExist($orgUnitId, $eventId){
 
 function enrollEngageEventUsers($orgUnitId, $sectionId, $usersToEnroll) {
     global $config;
-    foreach($usersToEnroll as $user){
+    foreach($usersToEnroll as $username){
+        $userId = doValenceRequest('GET', '/d2l/api/lp/'.$config['LP_Version'].'/users/?externalEmail'.$userEmail.'@localhost.local')['response'];
         $data = array(
             "UserId"=> $user
         );
+        echo $data['UserId'];
         $response = doValenceRequest('POST', '/d2l/api/lp/'.$config['LP_Version'].'/'.$orgUnitId.'/sections/'.$sectionId.'/enrollments/', $data); 
     }
 }
