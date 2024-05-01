c8befdca5e59ffd84c1f2530f643854184af51ae
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-05-01 11:16:58 -0400

    test

--- src/BLE.php
+++ src/BLE.php
@@ -139,6 +139,7 @@ function gradeEventAttendence($orgUnitId, $gradeId){
     
     foreach($eventAttendees as $userName){
         $user = doValenceRequest('GET', '/d2l/api/lp/'.$config['LP_Version'].'/users/?externalEmail='.$userName.'@localhost.local');
+        echo $user['response'];
         if($user['Code']==200){
             doValenceRequest('PUT', '/d2l/api/le/'.$config['LE_Version'].'/'.$orgUnitId.'/grades/'.$gradeId.'/values/'.$user['response'][0]->UserId, $data);
             echo '/d2l/api/le/'.$config['LE_Version'].'/'.$orgUnitId.'/grades/'.$gradeId.'/values/'.$user['response'][0]->UserId;
