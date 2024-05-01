4faf0ed67f947d1c75689c550be30aa4916328b3
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-05-01 16:31:47 -0400

    test

--- js/ble-bu.js
+++ js/ble-bu.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
       sectionId: document.getElementById("sessionIdToBedeleted").value
     };
     $.post('src/toolInteract.php', requestData, function(response){
-      document.getElementById("deleteConfirmModal").style.display="none";
+      document.getElementById("deleteConfirmModal").hide();
     }
     ).fail(function(xhr, status, error) {
       console.error('Error submitting form:', error);
