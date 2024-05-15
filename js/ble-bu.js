e17aca3a087c55d593e88561d2845aca2a42b116
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-05-15 14:39:19 -0400

    test

--- js/ble-bu.js
+++ js/ble-bu.js
@@ -4,6 +4,7 @@ let gradeItem = document.getElementById("gradeItem");
 let orgSelectTag = document.getElementById("ebuOrganization");
 let eventSelectTag = document.getElementById("ebuEvent");
 let responseContainer = document.getElementById("responseContainer");
+var loadingIcon = document.getElementById('loadingIcon');
 
 $(document).ready(function() {
   $('.select2').select2({
@@ -15,10 +16,12 @@ $(document).ready(function() {
   $('#ebuForm').submit(function(event) {
     event.preventDefault();
     var formData = $(this).serialize();
+    loadingIcon.innerHTML = '<img src="img/progress.gif" alt="Loading...">';
     // Make the POST request
     $.post('src/toolInteract.php', formData, function(response) {
       responseContainer.className = '';
       responseContainer.className = 'alert alert-success';
+      loadingIcon.innerHTML = '';
       responseContainer.innerHTML = response;
       responseContainer.focus();
       document.getElementById("ebuForm").reset();
--- src/home.php
+++ src/home.php
@@ -40,6 +40,7 @@
             <div class="mb-3">
               <button type="submit" class="btn btn-primary">Sync</button>
             </div>
+            <div id="loadingIcon"></div>
           </form>
           <div id="responseContainer" tabindex="-1"></div>
           <h2>Linked experience BU events</h2><br>
